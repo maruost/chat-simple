3a90f43b6d7397b48ccda19a11c287e447f2a855
maruost <64815309+maruost@users.noreply.github.com>
2021-06-30 15:27:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ChatScreen from "./components/ChatScreen/ChatScreen";
 import uniqid from "uniqid";
 
 function App() {
-  const id = localStorage.getItem("id");
+  const id = localStorage.getItem("id"); // необходимо для отображения сообщений пользователя в другом стиле
   if (!id) {
-    const uniq = uniqid();
+    const uniq = uniqid(); // если id нет, то генерируется 
     localStorage.setItem("id", uniq);
   }
 
